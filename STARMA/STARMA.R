a557abba715dc548acd84ee91e4b45f645dc1144
SK-WEN <13824856940@163.com>
2021-01-10 19:28:38 +0800

    finish "preprocess.R" and "STARMA.R"

--- STARMA/STARMA.R
+++ STARMA/STARMA.R
@@ -1,2 +1,42 @@
 ##STARMA model
-library(ggplot)
+library(starma)
+
+# Load spdep library to easily create weight matrices
+library(spdep)
+# Create a 5x5 regular grid which will be our lattice
+
+# Create a uniform first order neighbourhood
+plot(coordinate)
+knb <- dnearneigh(coordinate, 0, 135,longlat = TRUE)
+plot(knb, coordinate)
+# Lag the neighbourhood to create other order matrices
+knb <- nblag(knb, 1)
+klist <- list(order0=diag(25),
+order1=nb2mat(knb))
+
+# Simulate a STARMA(2;1) process
+eps <- matrix(rnorm(200*25), 200, 25)
+star <- eps
+for (t in 3:200) {
+star[t,] <- (.4*klist[[1]] + .25*klist[[2]]) %*% star[t-1,] +
+(.25*klist[[1]] ) %*% star[t-2,] +
+( - .3*klist[[2]]) %*% eps[t-1,] +
+eps[t, ]
+}
+star <- star[101:200,] # Remove first observations
+star <- stcenter(star) # Center and scale the dataset
+data_for_starma=stcenter(data)
+# Identify the process
+stacf(data, klist)
+stpacf(data, klist)
+# Estimate the process
+ar <- matrix(c(1, 1, 1, 0), 2, 2)
+
+ma <- matrix(c(0, 1), 1, 2)
+model <- starma(star, klist, ar, ma)
+model
+summary(model)
+# Diagnose the process
+stcor.test(model$residuals, klist, fitdf=4)
+stacf(model$residuals, klist)
+stpacf(model$residuals, klist)
--- pre-analysis/preprocess.R
+++ pre-analysis/preprocess.R
@@ -1,29 +1,40 @@
+source("C:\\Users\\13824\\OneDrive\\data\\frequently_used_graph.R")
 
-data=read.csv("C:\\Users\\13824\\OneDrive\\data\\data_filled.csv")
+data=read.csv("C:\\Users\\13824\\OneDrive\\data\\Wind_Spatio_Temporal_Dataset1\\Wind Spatio-Temporal Dataset1(2009, Ave).csv")
+coordinate=read.csv("C:\\Users\\13824\\OneDrive\\data\\Wind_Spatio_Temporal_Dataset1\\Wind Spatio-Temporal Dataset1(Coordinates).csv")
+coordinate=apply(coordinate,2,as.numeric)
 head(data)
-speed_data=data[,-seq(from = 3, to=dim(data)[2],by=2)]
+selection=sample(c(1:dim(data)[2]),25)
+data=data[,selection]
+coordinate=coordinate[selection,]
 
-library(ggplot2)
-X=rep(1:dim(speed_data)[1],dim(speed_data)[2]-1)
-Y=c(as.matrix(speed_data[,2:dim(speed_data)[2]]))
-group=rep(colnames(speed_data)[2:length(colnames(speed_data))],each=dim(speed_data)[1])
-data=cbind(X,Y,group)
-data=as.data.frame(data)
-data[,1]=as.numeric(data[,1])
-data[,2]=as.numeric(data[,2])
-str(data)
-data=data[-which(data[,'group']=='Mean_Speed_30_Turbine35'),]
-ggplot(data[8403:10402,],aes(x=X,y=Y,colour=group))+geom_point()
-ggplot(data,aes(x=group,y=Y))+geom_boxplot(aes(group=group))
+scatter_point(data)
+box_plot(data)
+cor_heat_map(data)
 
-data_for_heatMap=speed_data[,3:dim(speed_data)[2]]
-data_for_heatMap=apply(data_for_heatMap,2,as.numeric)
-cor_mat=cor(data_for_heatMap)
-library(reshape2)
-cor_mat<-melt(cor_mat)
-head(cor_mat)
 
-ggplot(data = cor_mat, aes(x=Var1, y=Var2, fill=value)) +
-  geom_tile()
+
+#library(ggplot2)
+#X=rep(1:dim(speed_data)[1],dim(speed_data)[2]-1)
+#Y=c(as.matrix(speed_data[,2:dim(speed_data)[2]]))
+#group=rep(colnames(speed_data)[2:length(colnames(speed_data))],each=dim(speed_data)[1])
+#data=cbind(X,Y,group)
+#data=as.data.frame(data)
+#data[,1]=as.numeric(data[,1])
+#data[,2]=as.numeric(data[,2])
+#str(data)
+#data=data[-which(data[,'group']=='Mean_Speed_30_Turbine35'),]
+#ggplot(data[8403:10402,],aes(x=X,y=Y,colour=group))+geom_point()
+#ggplot(data,aes(x=group,y=Y))+geom_boxplot(aes(group=group))
+#
+#data_for_heatMap=speed_data[,3:dim(speed_data)[2]]
+#data_for_heatMap=apply(data_for_heatMap,2,as.numeric)
+#cor_mat=cor(data_for_heatMap)
+#library(reshape2)
+#cor_mat<-melt(cor_mat)
+#head(cor_mat)
+#
+#ggplot(data = cor_mat, aes(x=Var1, y=Var2, fill=value)) +
+#  geom_tile()
 
 
